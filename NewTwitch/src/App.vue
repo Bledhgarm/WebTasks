0473d6d7aed8665715280553a24a99ee5c3af065
Bledhgarm <kurutta.grind@gmail.com>
2023-11-20 21:21:00 +0500

    Nothing has been done

---
+++ NewTwitch/src/App.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+    import search from './components/search.vue'
+</script>
+
+<template>
+    <search></search>
+</template>
+
+
---
+++ NewTwitch/src/components/search.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+
+<template>
+    <div class="search">
+        <input type="text" placeholder="Поиск">
+        <button></button>
+    </div>
+</template>
